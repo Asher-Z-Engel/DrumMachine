6eae9eea8ad5e897a8b11e572cbc735e6b303d2e
Asher <asherzengel@gmail.com>
2021-11-29 16:13:41 +0200

    Added keypress playing functionality

--- src/App.js
+++ src/App.js
@@ -14,32 +14,49 @@ import ClosedHH from './audio/CYCdh_K1close_ClHat-01.wav';
 import OpenHH from './audio/CYCdh_K1close_OpHat-01.wav';
 import Crash from './audio/CyCdh_K3Crash-02.wav';
 
-function App() {
-  const playSound = (event) => {
-    // console.dir(event.target.innerText);
-    let sample = document.getElementById(event.target.innerText);
-    console.dir(sample);
-    sample.play();
+const drumPadLetters = ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"];
+
+const getSampleId = (event) => {
+  let letter;
+  if (event.type === "click") {
+    letter = event.target.innerText;
+    playSound(letter);
+  } else if (event.type === "keydown") {
+    if (drumPadLetters.includes(event.key.toUpperCase())) {
+      letter = event.key.toUpperCase();
+      playSound(letter);
+    }
   }
+}
+
+const playSound = (sampleId) => {
+  let sample = document.getElementById(sampleId);
+  sample.play();
+}
+
+function App() {
+  
 
   return (
     <>
       <DrumMachine>
         <Display />
         <div className="drum-pads">
-          <DrumPad id="Kick" letter="Q" sound={Kick} playFunction={playSound}/>
-          <DrumPad id="Snare" letter="W" sound={Snare} playFunction={playSound} />
-          <DrumPad id="Snare-Rim" letter="E" sound={SnareRim} playFunction={playSound} />
-          <DrumPad id="Hi-Tom" letter="A" sound={HiTom} playFunction={playSound} />
-          <DrumPad id="Mid-Tom" letter="S" sound={MidTom} playFunction={playSound} />
-          <DrumPad id="Floor-Tom" letter="D" sound={LowTom} playFunction={playSound} />
-          <DrumPad id="Closed-Hats" letter="Z" sound={ClosedHH} playFunction={playSound} />
-          <DrumPad id="Open-Hats" letter="X" sound={OpenHH} playFunction={playSound} />
-          <DrumPad id="Crash" letter="C" sound={Crash} playFunction={playSound} />
+          <DrumPad id="Kick" letter="Q" sound={Kick} playFunction={getSampleId}/>
+          <DrumPad id="Snare" letter="W" sound={Snare} playFunction={getSampleId} />
+          <DrumPad id="Snare-Rim" letter="E" sound={SnareRim} playFunction={getSampleId} />
+          <DrumPad id="Hi-Tom" letter="A" sound={HiTom} playFunction={getSampleId} />
+          <DrumPad id="Mid-Tom" letter="S" sound={MidTom} playFunction={getSampleId} />
+          <DrumPad id="Floor-Tom" letter="D" sound={LowTom} playFunction={getSampleId} />
+          <DrumPad id="Closed-Hats" letter="Z" sound={ClosedHH} playFunction={getSampleId} />
+          <DrumPad id="Open-Hats" letter="X" sound={OpenHH} playFunction={getSampleId} />
+          <DrumPad id="Crash" letter="C" sound={Crash} playFunction={getSampleId} />
         </div>
       </DrumMachine>
     </>
   );
 }
+
+window.addEventListener('keydown', getSampleId)
 
 export default App;
